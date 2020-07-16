ae3c836fbb5f3709c4c1e903b02dcaf56e92dbd7
Jayme <43247281+Jxyme@users.noreply.github.com>
2020-07-16 16:34:32 +0100

    ✨ Initial commit

---
+++ index.js
@@ -0,0 +1,159 @@
+const request = require("request");
+const chalk = require('chalk');
+const fs = require("fs-extra");
+
+const config = require("./config.json");
+
+const SUCCESS = chalk.hex("#47FF56");
+const ERROR = chalk.hex("#FF0000");
+const WARN = chalk.hex("#FFAE42");
+const PROCESS = chalk.hex("#FFA500");
+const INFO = chalk.hex("#FF68F4");
+const LOG = chalk.hex("#35B8FF");
+
+const SURL = "https://discordapp.com/api/v6/users/@me/settings";
+
+setInterval(compareStats, 60000);
+
+/* Adds [LOG] and [time/date] prefix to all console.logs */
+
+let originalConsoleLog = console.log;
+console.log = function () {
+    args = [];
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    let hours = date.getUTCHours().toString().padStart(2, '0');
+    let minutes = date.getUTCMinutes().toString().padStart(2, '0');
+    let seconds = date.getUTCSeconds().toString().padStart(2, '0');
+
+    args.push(`${LOG(`[LOG]`)} ${INFO(`[${day}/${month}/${year} @ ${hours}:${minutes}:${seconds} UTC]`)}`);
+    for (var i = 0; i < arguments.length; i++) {
+        args.push(arguments[i]);
+    }
+    originalConsoleLog.apply(console, args);
+};
+
+/* Adds [WARN] and [time/date] prefix to all console.warns */
+
+let originalConsoleWarn = console.warn;
+console.warn = function () {
+    args = [];
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    let hours = date.getUTCHours().toString().padStart(2, '0');
+    let minutes = date.getUTCMinutes().toString().padStart(2, '0');
+    let seconds = date.getUTCSeconds().toString().padStart(2, '0');
+
+    args.push(`${WARN(`[WARN]`)} ${INFO(`[${day}/${month}/${year} @ ${hours}:${minutes}:${seconds} UTC]`)}`);
+    for (var i = 0; i < arguments.length; i++) {
+        args.push(arguments[i]);
+    }
+    originalConsoleWarn.apply(console, args);
+};
+
+/* Adds [ERROR] and [time/date] prefix to all console.errors */
+
+let originalConsoleError = console.error;
+console.error = function () {
+    args = [];
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    let hours = date.getUTCHours().toString().padStart(2, '0');
+    let minutes = date.getUTCMinutes().toString().padStart(2, '0');
+    let seconds = date.getUTCSeconds().toString().padStart(2, '0');
+
+    args.push(`${ERROR(`[ERROR]`)} ${INFO(`[${day}/${month}/${year} @ ${hours}:${minutes}:${seconds} UTC]`)}`);
+    for (var i = 0; i < arguments.length; i++) {
+        args.push(arguments[i]);
+    }
+    originalConsoleError.apply(console, args);
+};
+
+/* Start the Application and Check for Updates */
+
+console.log(SUCCESS("Application started successfully."));
+console.log(PROCESS("Fetching latest figures from NovelCOVID API..."));
+compareStats();
+
+/* PATCH request to Discord User, set Custom Status from provided text, emoji_id and emoji_name */
+
+function doRequest() {
+    let data = fs.readFileSync('./data.json', 'utf8');
+    let stats = JSON.parse(data);
+    return new Promise((resolve, reject) => {
+        request({
+            method: "PATCH",
+            uri: SURL,
+            headers: {
+                Authorization: config.token
+            },
+            json: {
+                custom_status: {
+                    text: "Today: " + formatNumber(stats.todayCases) + " Cases & " + formatNumber(stats.todayDeaths) + " Deaths",
+                    emoji_id: config.emojiID,
+                    emoji_name: config.emojiName
+                }
+            }
+        }, (err, res) => {
+            if (err) {
+                reject(ERROR("[ERROR] " + err));
+                return;
+            }
+            if (res.statusCode !== 200) {
+                reject(new Error(ERROR("[ERROR] Invalid Status Code: " + res.statusCode)));
+                return;
+            }
+            resolve(true);
+            console.log(LOG("[STATUS UPDATE] " + INFO("Today: " + formatNumber(stats.todayCases) + " Cases & " + formatNumber(stats.todayDeaths) + " Deaths")));
+        });
+    });
+}
+
+/* GET request to URL, fetches 'todayCases' and 'todayDeaths' from API response */
+
+function compareStats() {
+    let data = fs.readFileSync('./data.json', 'utf8');
+    let stats = JSON.parse(data);
+    request({
+        url: `https://disease.sh/v3/covid-19/countries/${config.country}`,
+        json: true
+    }, function (err, data) {
+        if (err) {
+            console.log(ERROR(err));
+        } else {
+            try {
+                if (stats.cases != data.body.cases && data.body.cases != undefined || stats.todayCases != data.body.todayCases && data.body.todayCases != undefined ||
+                    stats.deaths != data.body.deaths && data.body.deaths != undefined || stats.todayDeaths != data.body.todayDeaths && data.body.todayDeaths != undefined) {
+                    let coronaData = {
+                        cases: data.body.cases,
+                        todayCases: data.body.todayCases,
+                        deaths: data.body.deaths,
+                        todayDeaths: data.body.todayDeaths,
+                        recovered: data.body.recovered
+                    };
+                    let newData = JSON.stringify(coronaData, null, 4);
+                    fs.writeFileSync('./data.json', newData)
+                    console.log(LOG("[NEW] " + INFO("Figures have updated to: " + formatNumber(data.body.todayCases) + " Cases & " + formatNumber(data.body.todayDeaths) + " Deaths")));
+                    doRequest();
+                } else {
+                    return;
+                }
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    });
+}
+
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
