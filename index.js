5c0cf0388f940d46a42933758ae521a3170f87c9
Jayme <43247281+Jxyme@users.noreply.github.com>
2020-11-01 21:22:48 +0000

    ✨ Support for displaying yesterday's data, user-defined suffix's and more

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-const request = require("request");
+const request = require('request');
 const chalk = require('chalk');
-const fs = require("fs-extra");
+const fs = require('fs-extra');
 
-const config = require("./config.json");
+const config = require('./config.json');
 
-const SUCCESS = chalk.hex("#47FF56");
-const ERROR = chalk.hex("#FF0000");
-const WARN = chalk.hex("#FFAE42");
-const PROCESS = chalk.hex("#FFA500");
-const INFO = chalk.hex("#FF68F4");
-const LOG = chalk.hex("#35B8FF");
+const SUCCESS = chalk.hex('#43B581');
+const ERROR = chalk.hex('#F04747');
+const WARN = chalk.hex('#FAA61A');
+const PROCESS = chalk.hex('#F57731');
+const INFO = chalk.hex('#FF73FA');
+const LOG = chalk.hex('#44DDBF');
 
-const SURL = "https://discord.com/api/v6/users/@me/settings";
+const URL = `https://discord.com/api/v6/users/@me/settings`;
 
-setInterval(compareStats, 60000);
-
-/* Adds [LOG] and [time/date] prefix to all console.logs */
+/* Adds [LOG] and [dd/mm/yyyy | hh:mm:ss UTC] prefix to all console.log's */
 
 let originalConsoleLog = console.log;
 console.log = function () {
@@ -24,40 +22,17 @@ console.log = function () {
     let day = date.getDate();
     let month = date.getMonth() + 1;
     let year = date.getFullYear();
-
     let hours = date.getUTCHours().toString().padStart(2, '0');
     let minutes = date.getUTCMinutes().toString().padStart(2, '0');
     let seconds = date.getUTCSeconds().toString().padStart(2, '0');
-
-    args.push(`${LOG(`[LOG]`)} ${INFO(`[${day}/${month}/${year} @ ${hours}:${minutes}:${seconds} UTC]`)}`);
+    args.push(`${LOG(`[LOG]`)} ${INFO(`[${day}/${month}/${year} | ${hours}:${minutes}:${seconds} UTC]`)}`);
     for (var i = 0; i < arguments.length; i++) {
         args.push(arguments[i]);
     }
     originalConsoleLog.apply(console, args);
-};
+}
 
-/* Adds [WARN] and [time/date] prefix to all console.warns */
-
-let originalConsoleWarn = console.warn;
-console.warn = function () {
-    args = [];
-    let date = new Date();
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear();
-
-    let hours = date.getUTCHours().toString().padStart(2, '0');
-    let minutes = date.getUTCMinutes().toString().padStart(2, '0');
-    let seconds = date.getUTCSeconds().toString().padStart(2, '0');
-
-    args.push(`${WARN(`[WARN]`)} ${INFO(`[${day}/${month}/${year} @ ${hours}:${minutes}:${seconds} UTC]`)}`);
-    for (var i = 0; i < arguments.length; i++) {
-        args.push(arguments[i]);
-    }
-    originalConsoleWarn.apply(console, args);
-};
-
-/* Adds [ERROR] and [time/date] prefix to all console.errors */
+/* Adds [ERROR] and [dd/mm/yyyy | hh:mm:ss UTC] prefix to all console.error's */
 
 let originalConsoleError = console.error;
 console.error = function () {
@@ -66,93 +41,187 @@ console.error = function () {
     let day = date.getDate();
     let month = date.getMonth() + 1;
     let year = date.getFullYear();
-
     let hours = date.getUTCHours().toString().padStart(2, '0');
     let minutes = date.getUTCMinutes().toString().padStart(2, '0');
     let seconds = date.getUTCSeconds().toString().padStart(2, '0');
-
-    args.push(`${ERROR(`[ERROR]`)} ${INFO(`[${day}/${month}/${year} @ ${hours}:${minutes}:${seconds} UTC]`)}`);
+    args.push(`${ERROR(`[ERROR]`)} ${INFO(`[${day}/${month}/${year} | ${hours}:${minutes}:${seconds} UTC]`)}`);
     for (var i = 0; i < arguments.length; i++) {
         args.push(arguments[i]);
     }
     originalConsoleError.apply(console, args);
-};
+}
 
-/* Start the Application and Check for Updates */
+/* Format today's and yesterday's date in the following format: dd/mm/yyyy (e.g. 31/10/2020) */
 
-console.log(SUCCESS("Application started successfully."));
-console.log(PROCESS("Fetching latest figures from NovelCOVID API..."));
-compareStats();
+let today = new Date();
+today.setDate(today.getDate());
+let yesterday = new Date(today);
+yesterday.setDate(yesterday.getDate() - 1);
+let dd = today.getDate();
+let ydd = yesterday.getDate();
+let mm = today.getMonth() + 1;
+let ymm = yesterday.getMonth() + 1;
+let yy = today.getFullYear();
+let yyy = yesterday.getFullYear();
+if (dd < 10) {
+    dd = `0${dd}`;
+} else if (ydd < 10) {
+    ydd = `0${ydd}`;
+}
+if (mm < 10) {
+    mm = `0${mm}`;
+} else if (ymm < 10) {
+    ymm = `0${ymm}`;
+}
+today = `${dd}/${mm}/${yy}`;
+yesterday = `${ydd}/${ymm}/${yyy}`;
 
-/* PATCH request to Discord User, set Custom Status from provided text, emoji_id and emoji_name */
+console.log(SUCCESS(`Successfully started the application...`));
+console.log(PROCESS(`Fetching latest figures for ${today} (today) and ${yesterday} (yesterday)`));
 
-function doRequest() {
-    let data = fs.readFileSync('./data.json', 'utf8');
-    let stats = JSON.parse(data);
+let dataType;
+let figureOne;
+let figureTwo;
+let todayHasChanged;
+let yesterdayHasChanged;
+
+let initialTasks = [compareStatus, fetchToday, fetchYesterday, handleFigures],
+    i = 0;
+
+/* Consecutively execute initialTasks before handling the figures returned (onStartup only) */
+
+onStartup();
+
+function onStartup() {
+    initialTasks[i++]();
+    if (i < initialTasks.length) {
+        setTimeout(onStartup, 1500);
+    }
+}
+
+/* Execute the tasks at a specified interval (in milliseconds) */
+
+setInterval(function () {
+    let hour = new Date().getUTCHours();
+    let userPreference = config.showYesterday;
+    if (userPreference && hour >= 0 && hour < 3) {
+        y = 0;
+        todayHasChanged = 0;
+        yesterdayHasChanged = 0;
+        checkYesterday();
+    } else {
+        t = 0;
+        todayHasChanged = 0;
+        yesterdayHasChanged = 0;
+        checkToday();
+    }
+}, 60000); // 60 seconds
+
+/* Consecutively execute yesterdayTasks before handling the figures returned */
+
+let yesterdayTasks = [fetchToday, fetchYesterday, handleFigures],
+    y = 0;
+
+function checkYesterday() {
+    yesterdayTasks[y++]();
+    if (y < yesterdayTasks.length) {
+        setTimeout(checkYesterday, 1500);
+    }
+}
+
+/* Consecutively execute todayTasks before handling the figures returned */
+
+let todayTasks = [fetchToday, handleFigures],
+    t = 0;
+
+function checkToday() {
+    todayTasks[t++]();
+    if (t < todayTasks.length) {
+        setTimeout(checkToday, 1500);
+    }
+}
+
+/* Compare the user's Custom Status with the localData before attempting to make any changes */
+
+function compareStatus() {
+    let localData = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
     return new Promise((resolve, reject) => {
         request({
-            method: "PATCH",
-            uri: SURL,
+            method: 'GET',
+            uri: URL,
             headers: {
                 Authorization: config.token
             },
-            json: {
-                custom_status: {
-                    text: "Today: " + formatNumber(stats.todayCases) + " Cases & " + formatNumber(stats.todayDeaths) + " Deaths",
-                    emoji_id: config.emojiID,
-                    emoji_name: config.emojiName
-                }
-            }
+            json: true,
         }, (err, res) => {
             if (err) {
-                reject(ERROR("[ERROR] " + err));
-                return;
+                return reject(ERROR(`[ERROR] ${err}`));
             }
             if (res.statusCode !== 200) {
-                reject(new Error(ERROR("[ERROR] Invalid Status Code: " + res.statusCode)));
-                return;
+                return reject(ERROR(`[ERROR] Invalid Status Code: ${res.statusCode}`));
             }
             resolve(true);
-            console.log(LOG("[STATUS UPDATE] " + INFO("Today: " + formatNumber(stats.todayCases) + " Cases & " + formatNumber(stats.todayDeaths) + " Deaths")));
+            let customStatus = res.body.custom_status;
+            if (customStatus === null) {
+                console.log(WARN(`No already existing custom status was detected... setting user's status to the latest figures.`));
+                return resetLocalData();
+            }
+            let defaultString = {
+                'today': `Today: ${formatNumber(localData.today.todayCases)} Cases & ${formatNumber(localData.today.todayDeaths)} Deaths`,
+                'yesterday': `Yesterday: ${formatNumber(localData.yesterday.todayCases)} Cases & ${formatNumber(localData.yesterday.todayDeaths)} Deaths`
+            }
+            let suitableSuffix = config.suffix;
+            if (suitableSuffix && suitableSuffix.length > 75) {
+                throw new Error(ERROR(`Suffix provided exceeds the maximum character length of 75.`));
+            }
+            if ([defaultString.today, `${defaultString.today} ${config.suffix}`, defaultString.yesterday, `${defaultString.yesterday} ${config.suffix}`].indexOf(customStatus.text) < 0 ||
+                (customStatus.emoji_id !== config.emojiID) || (customStatus.emoji_name !== config.emojiName) || (localData.country !== config.country) || (localData.suffix !== config.suffix)) {
+                console.log(WARN(`User's custom status is not up-to-date... setting user's status to the latest figures.`));
+                return resetLocalData();
+            } else if (config.showYesterday && (customStatus.text === 'Today: 0 Cases & 0 Deaths')) {
+                console.log(WARN(`User's custom status is equal to today's figures, setting user's status to yesterday's figures.`));
+                return resetLocalData();
+            } else if (!config.showYesterday && ([defaultString.today, `${defaultString.today} ${config.suffix}`].indexOf(customStatus.text) < 0)) {
+                console.log(WARN(`User's custom status is not equal to today's figures, setting user's status to today's figures.`));
+                return resetLocalData();
+            } else {
+                console.log(WARN(`User's custom status is already up-to-date!`));
+                return;
+            }
         });
     });
 }
 
-/* GET request to URL, fetches 'todayCases' and 'todayDeaths' from API response */
+/* Fetch the latest figures for today and write them to localData */
 
-function compareStats() {
-    let data = fs.readFileSync('./data.json', 'utf8');
-    let stats = JSON.parse(data);
+function fetchToday() {
+    let localData = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
     request({
-        url: `https://disease.sh/v3/covid-19/countries/${config.country}`,
+        url: `https://disease.sh/v3/covid-19/countries/${config.country}?yesterday=false&twoDaysAgo=false&strict=true&allowNull=false`,
         json: true
     }, function (err, data) {
         if (err) {
-            console.log(ERROR(err));
+            console.error(err);
         } else {
-            let newestDate = Math.max(stats.updated, data.body.updated); /* Compare two dates and store the most recent in 'newestDate' */
-            if (newestDate <= stats.updated) { /* If newestDate is less or equal to the value in 'data.json' */
-                return; /* If yes, return */
+            let apiData = data.body;
+            let newestDate = Math.max(localData.today.updated, apiData.updated);
+            if (newestDate <= localData.today.updated) {
+                return todayHasChanged = 0;
             } else {
-                stats.updated = newestDate; /* Set the 'updated' value to newestDate */
-                fs.writeFileSync('./data.json', JSON.stringify(stats, null, 2)); /* Write the value to file */
+                localData.today.updated = newestDate;
+                fs.writeFileSync('./data.json', JSON.stringify(localData, null, 4));
                 try {
-                    if (stats.cases != data.body.cases && data.body.cases != undefined || stats.todayCases != data.body.todayCases && data.body.todayCases != undefined ||
-                        stats.deaths != data.body.deaths && data.body.deaths != undefined || stats.todayDeaths != data.body.todayDeaths && data.body.todayDeaths != undefined) {
-                        let coronaData = {
-                            updated: newestDate,
-                            cases: data.body.cases,
-                            todayCases: data.body.todayCases,
-                            deaths: data.body.deaths,
-                            todayDeaths: data.body.todayDeaths,
-                            recovered: data.body.recovered
-                        };
-                        let newData = JSON.stringify(coronaData, null, 4);
-                        fs.writeFileSync('./data.json', newData)
-                        console.log(LOG("[NEW] " + INFO("Figures have updated to: " + formatNumber(data.body.todayCases) + " Cases & " + formatNumber(data.body.todayDeaths) + " Deaths")));
-                        doRequest();
+                    if (localData.today.cases != apiData.cases || localData.today.todayCases != apiData.todayCases ||
+                        localData.today.deaths != apiData.deaths || localData.today.todayDeaths != apiData.todayDeaths ||
+                        localData.today.recovered != apiData.recovered) {
+                        for (let key in localData.today) {
+                            localData.today[key] = apiData[key];
+                        }
+                        fs.writeFileSync('./data.json', JSON.stringify(localData, null, 4));
+                        console.log(LOG('[TODAY] ' + WARN(`${formatNumber(apiData.todayCases)} Cases & ${formatNumber(apiData.todayDeaths)} Deaths`)));
+                        return todayHasChanged = 1;
                     } else {
-                        return;
+                        return todayHasChanged = 0;
                     }
                 } catch (err) {
                     console.error(err);
@@ -162,6 +231,124 @@ function compareStats() {
     });
 }
 
-function formatNumber(num) {
-    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+/* Fetch the latest figures for yesterday and write them to localData */
+
+function fetchYesterday() {
+    let localData = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+    request({
+        url: `https://disease.sh/v3/covid-19/countries/${config.country}?yesterday=true&twoDaysAgo=false&strict=true&allowNull=false`,
+        json: true
+    }, function (err, data) {
+        if (err) {
+            console.error(err);
+        } else {
+            let apiData = data.body;
+            let newestDate = Math.max(localData.yesterday.updated, apiData.updated);
+            if (newestDate <= localData.yesterday.updated) {
+                return yesterdayHasChanged = 0;
+            } else {
+                localData.yesterday.updated = newestDate;
+                fs.writeFileSync('./data.json', JSON.stringify(localData, null, 4));
+                try {
+                    if (localData.yesterday.cases != apiData.cases || localData.yesterday.todayCases != apiData.todayCases ||
+                        localData.yesterday.deaths != apiData.deaths || localData.yesterday.todayDeaths != apiData.todayDeaths ||
+                        localData.yesterday.recovered != apiData.recovered) {
+                        for (let key in localData.yesterday) {
+                            localData.yesterday[key] = apiData[key];
+                        }
+                        fs.writeFileSync('./data.json', JSON.stringify(localData, null, 4));
+                        console.log(LOG('[YESTERDAY] ' + WARN(`${formatNumber(apiData.todayCases)} Cases & ${formatNumber(apiData.todayDeaths)} Deaths`)));
+                        return yesterdayHasChanged = 1;
+                    } else {
+                        return yesterdayHasChanged = 0;
+                    }
+                } catch (err) {
+                    console.error(err);
+                }
+            }
+        }
+    });
+}
+
+/* Handle localData before attempting to update the user's Custom Status */
+
+function handleFigures() {
+    let localData = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+    let userPreference = config.showYesterday;
+    if (userPreference) {
+        if (yesterdayHasChanged && (localData.today.todayCases === 0 && localData.today.todayDeaths === 0) ||
+            todayHasChanged && (localData.today.todayCases === 0 && localData.today.todayDeaths === 0)) {
+            dataType = 'Yesterday';
+            figureOne = formatNumber(localData.yesterday.todayCases);
+            figureTwo = formatNumber(localData.yesterday.todayDeaths);
+            return updateStatus();
+        } else if (todayHasChanged && (localData.today.todayCases !== 0 || localData.today.todayDeaths !== 0)) {
+            dataType = 'Today';
+            figureOne = formatNumber(localData.today.todayCases);
+            figureTwo = formatNumber(localData.today.todayDeaths);
+            return updateStatus();
+        } else {
+            return;
+        }
+    } else {
+        if (todayHasChanged) {
+            dataType = 'Today';
+            figureOne = formatNumber(localData.today.todayCases);
+            figureTwo = formatNumber(localData.today.todayDeaths);
+            return updateStatus();
+        } else {
+            return;
+        }
+    }
+}
+
+/* Update user's Custom Status */
+
+function updateStatus() {
+    return new Promise((resolve, reject) => {
+        request({
+            method: 'PATCH',
+            uri: URL,
+            headers: {
+                Authorization: config.token
+            },
+            json: {
+                custom_status: {
+                    text: `${dataType}: ${figureOne} Cases & ${figureTwo} Deaths ${(config.suffix ? `${config.suffix}` : '')}`,
+                    emoji_id: config.emojiID,
+                    emoji_name: config.emojiName
+                }
+            }
+        }, (err, res) => {
+            if (err) {
+                return reject(ERROR(`[ERROR] ${err}`));
+            }
+            if (res.statusCode !== 200) {
+                return reject(ERROR(`[ERROR] Invalid Status Code: ${res.statusCode}`));
+            }
+            resolve(true);
+            console.log(LOG('[UPDATED STATUS] ' + WARN(`${dataType}: ${figureOne} Cases & ${figureTwo} Deaths ${(config.suffix ? `${config.suffix}` : '')}`)))
+        });
+    });
+}
+
+/* Reset localData */
+
+function resetLocalData() {
+    let localData = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+    for (let key in localData.today) {
+        localData.today[key] = 0;
+    }
+    for (let key in localData.yesterday) {
+        localData.yesterday[key] = 0;
+    }
+    localData.country = config.country;
+    localData.suffix = config.suffix;
+    fs.writeFileSync('./data.json', JSON.stringify(localData, null, 4));
+}
+
+/* Format figureOne and figureTwo if they are > 999  (e.g. 23065 → 23,065) */
+
+function formatNumber(figure) {
+    return figure.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 }
